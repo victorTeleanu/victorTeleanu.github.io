e84c2d1f8046b37778c893729e47b296c0edb2d3
Victor Teleanu <alexteleanu9@gmail.com>
2024-11-06 19:42:02 +0100

    Mejoras en modo claro/oscuro

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
-// 1. MODO OSCURO / MODO CLARO
+// 1. MODO OSCURO / CLARO
 
+// Configuración inicial de la clase de tema antes de renderizar
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
-    document.documentElement.classList.add('dark-mode');
+    document.documentElement.className = 'dark-mode'; // Configura dark-mode inmediatamente
 }
 
+// Esperar a que cargue la página para inicializar el evento de alternancia y la imagen
 window.onload = function() {
-    // Referencia al botón del toggle
-    const button = document.getElementById('mode-button');
+    const modeIcon = document.getElementById('mode-toggle');
 
-    // Función para cambiar el tema
-    button.addEventListener('click', () => {
-        // Alterna la clase dark-mode en el body
-        document.documentElement.classList.toggle('dark-mode');
+    // Establecer la imagen correcta según el tema almacenado
+    if (savedTheme === 'dark') {
+        modeIcon.src = 'files/modo_oscuro.jpg';
+    } else {
+        modeIcon.src = 'files/modo_claro.jpg';
+    }
 
-        // Guarda la preferencia del usuario en localStorage
-        if (document.documentElement.classList.contains('dark-mode')) {
-            localStorage.setItem('theme', 'dark');
-            document.documentElement.style.transition = 'background-color 0.5s ease-in';
-        } else {
+    // Evento de clic para alternar entre modos
+    modeIcon.addEventListener('click', () => {
+        if (document.documentElement.className === 'dark-mode') {
+            document.documentElement.className = ''; // Quita dark-mode
+            modeIcon.src = 'files/modo_claro.jpg';
             localStorage.setItem('theme', 'light');
-            document.documentElement.style.transition = 'background-color 0.5s ease-in';
+        } else {
+            document.documentElement.className = 'dark-mode'; // Activa dark-mode
+            modeIcon.src = 'files/modo_oscuro.jpg';
+            localStorage.setItem('theme', 'dark');
         }
     });
-}
+};
+
+
+// 2.
